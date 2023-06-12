912b00678d62a392c4550287ab4dfd2627546fbe
Sahad Mahaboob <sahadmahaboob@gmail.com>
2023-06-12 15:19:15 +0530

    Added image slider

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Script for navigation bar
+
+const productContainers = [...document.querySelectorAll('.pro-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
