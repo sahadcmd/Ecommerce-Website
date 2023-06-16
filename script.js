5b5035f7d1c870c1dc1b3e9b5d993649089aa11b
Sahad Mahaboob <sahadmahaboob@gmail.com>
2023-06-16 23:47:42 +0530

    Home page made responsive

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
 
+if(bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+  })
+};
 
-const productContainers = [...document.querySelectorAll('.pro-container')];
-const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
-const preBtn = [...document.querySelectorAll('.pre-btn')];
-
-productContainers.forEach((item, i) => {
-    let containerDimensions = item.getBoundingClientRect();
-    let containerWidth = containerDimensions.width;
-
-    nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += containerWidth;
-    })
-
-    preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= containerWidth;
-    })
-})
+if(close) {
+  close.addEventListener('click', () => {
+    nav.classList.remove('active');
+  })
+};
